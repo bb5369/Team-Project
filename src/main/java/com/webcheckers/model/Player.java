012648771f3c7e7400a8bac8cc6ef64c8953cea7
Rohandeep <rs9773@g.rit.edu>
2018-03-01 09:00:02 -0500

    added java docs to the Player.java

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -4,12 +4,23 @@ package com.webcheckers.model;
  * Model entity for a checkers Player
  */
 public class Player {
+	//instance variables
 	private String name;
-
+	
+	/**
+	 * Parameterize constructor
+	 * This constructor intializes the name of the player
+	 * @param name: Player name String
+	 */
 	public Player(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * getName method
+	 * This method is used to get player's name
+	 * @return a string containing player's name.
+	 */
 	public String getName() {
 		return this.name;
 	}
