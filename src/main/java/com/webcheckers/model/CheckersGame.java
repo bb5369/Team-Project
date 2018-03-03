3dad6980493ba55fb188cc8cee353da4a05929ca
Ian Flournoy <ian@flournoy.us>
2018-03-02 19:57:35 -0500

    Added CheckersGame model skeleton

---
+++ src/main/java/com/webcheckers/model/CheckersGame.java
@@ -0,0 +1,12 @@
+package com.webcheckers.model;
+
+public class CheckersGame {
+
+	private final Player playerRed;
+	private final Player playerWhite;
+
+	public CheckersGame(Player playerRed, Player playerWhite) {
+		this.playerRed = playerRed;
+		this.playerWhite = playerWhite;
+	}
+}
