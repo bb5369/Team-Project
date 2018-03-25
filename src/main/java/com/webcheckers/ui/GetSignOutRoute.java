fe5ac6fcaae68ea49ad07c931d20c848fcdfbb5c
adh9570 <adh9570@rit.edu>
2018-03-24 23:43:00 -0400

    Created constructor to initialize template engine

--- src/main/java/com/webcheckers/ui/GetSignOutRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignOutRoute.java
@@ -1,8 +1,27 @@
 package com.webcheckers.ui;
 
 
-public class GetSignOutRoute {
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
 
-    
+import java.util.Objects;
 
+public class GetSignOutRoute implements Route {
+
+    static final String TITLE = "Player Sign Out";
+    static final String VIEW_NAME = "signout.ftl";
+
+    private final TemplateEngine templateEngine;
+
+    GetSignOutRoute(final TemplateEngine templateEngine){
+        Objects.requireNonNull(templateEngine, "Template Engine must not be null.");
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
 }
