19beb1b81ef72df2eb723f35d57ce2de4c68c8a5
Ian Flournoy <ian@flournoy.us>
2018-03-29 18:27:13 -0400

    Updated SpaceTest to work with new state based logic

--- src/test/java/com/webcheckers/model/SpaceTest.java
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -3,6 +3,7 @@ package com.webcheckers.model;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -11,52 +12,111 @@ import org.junit.jupiter.api.Test;
 public class SpaceTest {
 
 	private static final int SPACE_ID = 0;
+	private static Piece mockPiece;
 
-	/**
-	 * Test that the single-arg constructor works without failure.
-	 */
-	@Test
-	public void constructor_oneArg() {
-		new Space(SPACE_ID);
+
+	@BeforeAll
+	public static void setupMocks() {
+		// internal model mocks
+		mockPiece = mock(Piece.class);
 	}
 
 	/**
-	 * Test that two arg constructor works without failure.
+	 * Test that the constructor using a piece works as expected
 	 */
 	@Test
-	public void constructor_twoArg() {
-		Piece mockPiece = mock(Piece.class);
-
+	public void constructor_piece() {
 		new Space(SPACE_ID, mockPiece);
 	}
 
+
+
 	/**
-	 * Test that the space is not valid for moving
+	 * Test that the state constructor works as expected
+	 */
+	@Test
+	public void constructor_state() {
+		new Space(SPACE_ID, Space.State.INVALID);
+	}
+
+	/**
+	 * Test that the piece constructors throws a null exception
+	 */
+	@Test
+	public void constructor_null_piece() {
+		Piece nullPiece = null;
+
+		assertThrows(NullPointerException.class, () -> {
+			new Space(SPACE_ID, nullPiece);
+		});
+	}
+
+	/**
+	 * Test that the space is not valid for a piece to move to
 	 */
 	@Test
 	public void invalidSpace() {
-		Space testSpace = new Space(SPACE_ID);
+		Space testSpace = new Space(SPACE_ID, Space.State.INVALID);
 
 		assertFalse(testSpace.isValid());
 	}
 
 	/**
-	 * Test that the space is actually valid
+	 * Test that the space is open
 	 */
 	@Test
-	public void validSpace() {
-		Piece mockPiece = mock(Piece.class);
+	public void validSpacePiece() {
 		Space testSpace = new Space(SPACE_ID, mockPiece);
 
 		assertTrue(testSpace.isValid());
 	}
 
 	/**
-	 * Test that the Space maintains its cell ID
+	 * Test if a space is open for a piece to move to
 	 */
 	@Test
-	public void cellId() {
-		Space testSpace = new Space(SPACE_ID);
+	public void isOpen() {
+		Space testSpace = new Space(SPACE_ID, Space.State.OPEN);
+
+		assertTrue(testSpace.isOpen());
+	}
+
+	/**
+	 * Test if a space is not open
+	 */
+	@Test
+	public void isNotOpen() {
+		Space testSpace = new Space(SPACE_ID, mockPiece);
+
+		assertFalse(testSpace.isOpen());
+	}
+
+	/**
+	 * Test if a space is occupied
+	 */
+	@Test
+	public void isOccupied() {
+		Space testSpace = new Space(SPACE_ID, mockPiece);
+
+		assertTrue(testSpace.isOccupied());
+	}
+
+	/**
+	 * Test if a space is not occupied
+	 */
+	@Test
+	public void isNotOccupied() {
+		Space testSpace = new Space(SPACE_ID, Space.State.OPEN);
+
+		assertFalse(testSpace.isOccupied());
+	}
+
+	/**
+	 * Test that a space retains its cell ID
+	 */
+	@Test
+	public void cellIdAmnesia() {
+		Space testSpace = new Space(SPACE_ID, Space.State.OPEN);
 
 		assertEquals(SPACE_ID, testSpace.getCellIdx());
 	}
@@ -65,10 +125,19 @@ public class SpaceTest {
 	 * Test that the Space returns a piece
 	 */
 	@Test
-	public void spaceOccupied() {
-		Piece mockPiece = mock(Piece.class);
+	public void spacePiece() {
 		Space testSpace = new Space(SPACE_ID, mockPiece);
 
 		assertSame(mockPiece, testSpace.getPiece());
+	}
+
+	/**
+	 * Test that a Space knows its state
+	 */
+	@Test
+	public void spaceState() {
+		Space testSpace = new Space(SPACE_ID, mockPiece);
+
+		assertSame(Space.State.OCCUPIED, testSpace.getState());
 	}
 }
