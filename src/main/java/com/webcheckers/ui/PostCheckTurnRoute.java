f97cc5413faea34a93b7c7ec50f23d5a318acfd4
adh9570 <adh9570@rit.edu>
2018-04-01 20:17:15 -0400

    Refactored and added documentation

--- src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
@@ -15,47 +15,57 @@ import com.google.gson.Gson;
 import java.util.Objects;
 import java.util.logging.Logger;
 
+/**
+ * Checks if it's a player's turn or not
+ */
 public class PostCheckTurnRoute implements Route {
     private static final Logger LOG = Logger.getLogger(PostCheckTurnRoute.class.getName());
-    private final TemplateEngine templateEngine;
 
     private final GameManager gameManager;
-    private final PlayerLobby playerLobby;
 
     private final String opponentResigned = "true"/*"Your opponent has resigned From the game"*/;
 
     private final Gson gson;
 
-    public PostCheckTurnRoute(TemplateEngine templateEngine, PlayerLobby playerLobby, GameManager gameManager, Gson gson) {
+    /**
+     * Initializes the PostCheckTurnRoute
+     *
+     * @param gameManager - used to find a game
+     * @param gson - used to transmit messages to AJAX
+     */
+    public PostCheckTurnRoute(GameManager gameManager, Gson gson) {
         // validation
-        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
-        Objects.requireNonNull(playerLobby, "player must not be null");
         Objects.requireNonNull(gameManager, "gameManager must not be null");
         Objects.requireNonNull(gson, "gson must not be null");
 
-        this.templateEngine = templateEngine;
-        this.playerLobby = playerLobby;
         this.gameManager = gameManager;
         this.gson = gson;
 
         LOG.config("PostCheckTurnRoute is initialized");
     }
 
+    /**
+     * Determines if a player is resigning
+     *
+     * @param request - the HTTP request
+     * @param response - the HTTP response
+     * @return - null or a Json message
+     */
     @Override
-    public Object handle(Request request, Response response){
+    public Object handle(Request request, Response response) {
         LOG.finer("PostCheckTurnRoute is invoked.");
 
-        Player currrentPlayer = request.session().attribute("Player");
+        Player currentPlayer = request.session().attribute("Player");
         CheckersGame game;
-        if(gameManager.isPlayerInAResignedGame(currrentPlayer))
-            game = gameManager.getResignedGame(currrentPlayer);
+        if (gameManager.isPlayerInAResignedGame(currentPlayer))
+            game = gameManager.getResignedGame(currentPlayer);
         else
             game = null;
-        if(game != null){
+        if (game != null) {
             Message message = new Message(opponentResigned, Message.MessageType.info);
-            gameManager.clearGame(currrentPlayer);
-            gameManager.clearResigned(currrentPlayer);
-            if(game.isResignedPlayer(currrentPlayer))
+            gameManager.clearGame(currentPlayer);
+            gameManager.clearResigned(currentPlayer);
+            if (game.isResignedPlayer(currentPlayer))
                 response.redirect(WebServer.HOME_URL);
             else
                 return formatMessageJson(message);
@@ -65,7 +75,8 @@ public class PostCheckTurnRoute implements Route {
 
     /**
      * formatMessageJson - Format text and a message type as JSON for use in returning to the frontend
-     * @return gson Message object
+     *
+     * @return - gson Message object
      */
     public Object formatMessageJson(Message message) {
         return gson.toJson(message);
