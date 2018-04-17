4429418c4b1e9ccf134afe553ae3e2d142cb345b
Ian Flournoy <ian@flournoy.us>
2018-04-17 17:49:55 -0400

    Don't log exceptions when they happen in default

--- src/main/java/com/webcheckers/model/CheckersGame.java
+++ src/main/java/com/webcheckers/model/CheckersGame.java
@@ -145,8 +145,6 @@ public class CheckersGame {
             builder = (CheckersBoardBuilder)boardBuilderMethod.invoke(null);
 
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)  {
-            System.out.println(e.toString());
-            e.printStackTrace();
             builder = CheckersBoardBuilder.aStartingBoard();
         }
 
